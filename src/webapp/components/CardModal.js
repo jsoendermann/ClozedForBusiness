0b6bbdcd3a103fb6e1265c6f03f8245b38a8d76a
Jan Soendermann <jan.soendermann+git@gmail.com>
2016-12-20 07:29:31 +0100

    More instructions

--- src/webapp/components/CardModal.js
+++ src/webapp/components/CardModal.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { Input, Modal } from 'antd';
 import './CardModal.scss';
 
-const CardModal = (props) => (
+const CardModal = props => (
   <Modal
     title="Cards"
     okText="Clear text & cards"
     cancelText="Cancel"
-    closable={true}
+    closable
     {...props}
-    >
-    <p id="import-instructions">Copy the following text into a .tsv file and import into <a href="/deck.apkg">this Anki deck</a>.</p>
+  >
+    <p id="import-instructions">Copy the text below into a .tsv file and import it into <a href="/deck.apkg">this Anki deck</a>. Make sure to select "Tab" as field separator and enable "Allow HTML in fields" in the import dialog.</p>
     <Input id="generated-cards-input" value={props.generatedCards} type="textarea" />
   </Modal>
 );
